ac24e149c2423e2073f3ba3d4a05d4b24e3e0da7
Jedrulcia <jedrzej.koriat@gmail.com>
2025-03-07 00:31:27 +0100

    Added: scrapping sherdog link

--- Services/WikipediaService.cs
+++ Services/WikipediaService.cs
@@ -8,10 +8,9 @@ namespace UfcStatsAPI.Services
 	public class WikipediaService
 	{
 		private static readonly HttpClient httpClient = new HttpClient();
-		private async Task ScrapUfcRankingsPage()
+		private async static Task<List<HtmlNode>> DownloadWeightClassRankings()
 		{
 			var url = "https://en.wikipedia.org/wiki/UFC_rankings";
-
 
 			var response = await httpClient.GetStringAsync(url);
 
@@ -27,16 +26,12 @@ namespace UfcStatsAPI.Services
 				desiredTables.Add(tables[i]);
 			}
 
-			/*
-			 * 3 - heavyweight
-			 * 4 - light heavyweight
-			 * 5 - middleweight
-			 * 6 - welterweight
-			 * 7 - lightweight
-			 * 8 - featherweight
-			 * 9 - bantamweight
-			 * 10 - flyweight
-			 */
+			return desiredTables;
+		}
+
+		private async static Task<Dictionary<string, List<string>>> ScrapSherdogLinks(List<HtmlNode> desiredTables)
+		{
+
 			List<string> tablesNames = ["heavyweight", "light heavyweight", "middleweight", "welterweight", "lightweight", "featherweight", "bantamweight", "flyweight"];
 
 			Dictionary<string, List<string>> weightClasses = new Dictionary<string, List<string>>();
@@ -45,7 +40,7 @@ namespace UfcStatsAPI.Services
 
 			for (int i = 0; i < desiredTables.Count; i++)
 			{
-				List<string> fighters = new List<string>();
+				List<string> sherdogLinks = new List<string>();
 
 				string tableHtml = desiredTables[i].OuterHtml;
 				string[] lines = tableHtml.Split(new char[] { '\n' }, StringSplitOptions.None);
@@ -57,13 +52,61 @@ namespace UfcStatsAPI.Services
 						var match = Regex.Match(lines[j + 2], pattern);
 						if (match.Success)
 						{
-							fighters.Add(match.ToString());
+							string? sherdog = await ScrapSingleSherdogLink(match.ToString());
+							if (sherdog != null)
+							{
+								await ScrapSherdogStats(sherdog);
+								sherdogLinks.Add(sherdog.ToString());
+							}
 						}
 					}
 				}
-
-				weightClasses.Add(tablesNames[i], fighters);
+				weightClasses.Add(tablesNames[i], sherdogLinks);
 			}
+
+			return weightClasses;
+		}
+
+		private async static Task<string?> ScrapSingleSherdogLink(string link)
+		{
+			var url = "https://en.wikipedia.org" + link;
+
+			var response = await httpClient.GetStringAsync(url);
+
+			var htmlDoc = new HtmlDocument();
+			htmlDoc.LoadHtml(response);
+
+			var table = htmlDoc.DocumentNode.SelectSingleNode("//table[@class='infobox vcard']");
+
+			var name = Regex.Match(table.OuterHtml, @"<span class=""fn"">([^<]+)<\/span>");
+			var match = Regex.Match(table.OuterHtml, "http://www\\.sherdog\\.com/fightfinder/fightfinder\\.asp\\?fighterID=\\d+");
+
+			if (match.Success && name.Success)
+			{
+
+
+				string modifiedName = name.ToString().Replace(' ', '-');
+				modifiedName = modifiedName.Substring(0, modifiedName.Length - 7);
+				int biggerSignIndex = modifiedName.LastIndexOf('>');
+				modifiedName = modifiedName.Substring(biggerSignIndex + 1);
+
+				int equalSignIndex = match.ToString().LastIndexOf('=');
+				string fighterId = match.ToString().Substring(equalSignIndex + 1);
+
+				return "https://www.sherdog.com/fighter/" + modifiedName + '-' + fighterId;
+			}
+			return null;
+		}
+		private async static Task ScrapSherdogStats(string url)
+		{
+
+			var response = await httpClient.GetStringAsync(url);
+
+			var htmlDoc = new HtmlDocument();
+			htmlDoc.LoadHtml(response);
+
+			var bio = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='bio-holder']");
+			var record = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='winsloses-holder']");
 		}
 	}
 }
