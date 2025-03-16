0b274914deace6a7a40a8e244f06398ff7ad89d8
Jedrulcia <jedrzej.koriat@gmail.com>
2025-03-16 22:10:30 +0100

    Add: Scrapping bio, title, wins, loses (fight history left)

--- Services/WikipediaService.cs
+++ Services/WikipediaService.cs
@@ -44,6 +44,7 @@ namespace UfcStatsAPI.Services
 				// Splitting single table html to lines
 				string[] lines = rankingTables[i].OuterHtml.Split(new char[] { '\n' }, StringSplitOptions.None);
 
+				// List of tasks - max 16 fighters at one time
 				SemaphoreSlim semaphore = new SemaphoreSlim(16);
 				List<Task> tasks = new List<Task>();
 
@@ -57,7 +58,8 @@ namespace UfcStatsAPI.Services
 						if (figtherWikipediaLink.Success)
 						{
 							await semaphore.WaitAsync();
-							tasks.Add(Task.Run(async () => {
+							tasks.Add(Task.Run(async () =>
+							{
 								// Searching for sherdog link on fighters wikipedia page
 								string? fighterSherdogLink = await ScrapSingleSherdogLink(figtherWikipediaLink.ToString());
 
@@ -135,46 +137,139 @@ namespace UfcStatsAPI.Services
 			// Age, Height
 			var bio = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='bio-holder']").OuterHtml.Split(new char[] { '\n' }, StringSplitOptions.None);
 
-			/*for (int i = 0; i < bio.Length; i++)
+			for (int i = 0; i < bio.Length; i++)
 			{
 				if (bio[i].Contains("AGE"))
 				{
-					// BAD
-					fighter.Add("Age", Convert.ToInt32(bio[i + 2].Substring(3, 2)));
+					// Age
+					int age = Convert.ToInt32(Regex.Match(bio[i + 1], @"<td><b>(\d+)").ToString().Substring(7));
+					fighter.Add("Age", age);
 				}
 				if (bio[i].Contains("HEIGHT"))
 				{
-					// BAD
-					fighter.Add("Height", Convert.ToInt32(bio[i + 4].Substring(2, 3)));
+					// Height
+					int height = Convert.ToInt32(Regex.Match(bio[i], @"(\d+\.\d+)\s*cm").ToString().Substring(0, 3));
+					fighter.Add("Height", height);
 				}
-			}*/
+			}
 
 			// Name + Surname, Nickname, Country
 			var fighterTitle = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='fighter-title']").OuterHtml.Split(new char[] { '\n' }, StringSplitOptions.None);
 
-			/*for (int i = 0; i < fighterTitle.Length; i++)
+			for (int i = 0; i < fighterTitle.Length; i++)
 			{
+				if (fighterTitle[i].Contains("fighter-nationality"))
+				{
+					// Country
+					string country = Regex.Match(fighterTitle[i + 2], @">(.*?)<").ToString().TrimEnd('<').Substring(1);
+					fighter.Add("Country", country);
+				}
 				if (fighterTitle[i].Contains("fn"))
 				{
-					// Name
+					// Name and Surname
+					string name = Regex.Match(fighterTitle[i], @"fn(.*?)<").ToString().TrimEnd('<').Substring(4);
+					fighter.Add("Name", name);
 				}
 				if (fighterTitle[i].Contains("nickname"))
 				{
 					// Nickname
+					string nickname = Regex.Match(fighterTitle[i], @"<em>(.*?)<").ToString().TrimEnd('<').Substring(4);
+					fighter.Add("Nickname", nickname);
 				}
-				if (fighterTitle[i].Contains("fighter-nationality"))
-				{
-					// Country
-				}
-			}*/
+			}
 
-			// Wins, WinsKo, WinsSub, WinsDec, WinsOthers, Losses, LossesKo, LossesSub, LossesDec, LossesOthers, NoContest
+			// Wins, WinsKo, WinsSub, WinsDec, WinsOthers
 			var record = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='winsloses-holder']");
 
-			var wins = record.SelectSingleNode("//div[@class='wins']");
-			var loses = record.SelectSingleNode("//div[@class='loses']");
+			var winsHtml = record.SelectSingleNode("//div[@class='wins']").OuterHtml.Split(new char[] { '\n' }, StringSplitOptions.None);
 
+			// Initialize fields that sometimes are not present
+			int winOthers = 0;
 
+			for (int i = 0; i < winsHtml.Count(); i++)
+			{
+				if (winsHtml[i].Contains(">Wins<"))
+				{
+					// Wins
+					int wins = Convert.ToInt32(Regex.Match(winsHtml[i + 1], @">(\d+)<").ToString().Trim('<').Substring(1));
+					fighter.Add("Wins", wins);
+				}
+				if (winsHtml[i].Contains("em> TKO"))
+				{
+					// Wins by KO/TKO
+					int winKo = Convert.ToInt32(Regex.Match(winsHtml[i + 2], @">(\d+)<").ToString().Trim('<').Substring(1));
+					fighter.Add("WinKO", winKo);
+				}
+				if (winsHtml[i].Contains(">SUBMISSIONS"))
+				{
+					// Wins by Submission
+					int winSub = Convert.ToInt32(Regex.Match(winsHtml[i + 2], @">(\d+)<").ToString().Trim('<').Substring(1));
+					fighter.Add("WinSUB", winSub);
+				}
+				if (winsHtml[i].Contains(">DECISIONS"))
+				{
+					// Wins by Decision
+					int winDec = Convert.ToInt32(Regex.Match(winsHtml[i + 2], @">(\d+)<").ToString().Trim('<').Substring(1));
+					fighter.Add("WinDEC", winDec);
+				}
+				if (winsHtml[i].Contains(">OTHERS"))
+				{
+					// Other wins
+					winOthers = Convert.ToInt32(Regex.Match(winsHtml[i + 2], @">(\d+)<").ToString().Trim('<').Substring(1));
+				}
+			}
+
+			// Add fields that sometimes are not present
+			fighter.Add("WinOTH", winOthers);
+
+			// Losses, LossesKo, LossesSub, LossesDec, LossesOthers, NoContest
+			var lossesHtml = record.SelectSingleNode("//div[@class='loses']").OuterHtml.Split(new char[] { '\n' }, StringSplitOptions.None);
+
+			// Initialize fields that sometimes are not present
+			int loseOthers = 0;
+			int noContest = 0;
+
+			for (int i = 0; i < lossesHtml.Count(); i++)
+			{
+				if (lossesHtml[i].Contains(">Losses<"))
+				{
+					// Loses
+					int losses = Convert.ToInt32(Regex.Match(lossesHtml[i + 1], @">(\d+)<").ToString().Trim('<').Substring(1));
+					fighter.Add("Losses", losses);
+				}
+				if (lossesHtml[i].Contains("em> TKO"))
+				{
+					// Lose by KO/TKO
+					int winKo = Convert.ToInt32(Regex.Match(lossesHtml[i + 2], @">(\d+)<").ToString().Trim('<').Substring(1));
+					fighter.Add("LoseKO", winKo);
+				}
+				if (lossesHtml[i].Contains(">SUBMISSIONS"))
+				{
+					// Lose by Submission
+					int loseSub = Convert.ToInt32(Regex.Match(lossesHtml[i + 2], @">(\d+)<").ToString().Trim('<').Substring(1));
+					fighter.Add("LoseSUB", loseSub);
+				}
+				if (lossesHtml[i].Contains(">DECISIONS"))
+				{
+					// Lose by Decision
+					int loseDec = Convert.ToInt32(Regex.Match(lossesHtml[i + 2], @">(\d+)<").ToString().Trim('<').Substring(1));
+					fighter.Add("LoseDEC", loseDec);
+				}
+				if (lossesHtml[i].Contains(">OTHERS"))
+				{
+					// Other loses
+					loseOthers = Convert.ToInt32(Regex.Match(lossesHtml[i + 2], @">(\d+)<").ToString().Trim('<').Substring(1));
+				}
+				if (lossesHtml[i].Contains("winloses nc"))
+				{
+					// No contest
+					noContest = Convert.ToInt32(Regex.Match(lossesHtml[i + 2], @">(\d+)<").ToString().Trim('<').Substring(1));
+				}
+			}
+
+			// Add fields that sometimes are not present
+			fighter.Add("LoseOTH", loseOthers);
+			fighter.Add("NoContest", noContest);
 
 			// Fights: Result, Opponent, Event, Date, Method, Round, Time
 			var fightHistory = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='module fight_history']").OuterHtml.Split(new char[] { '\n' }, StringSplitOptions.None);
@@ -182,25 +277,37 @@ namespace UfcStatsAPI.Services
 			return fighter;
 		}
 
+		// Create a json structure (see template.json)
 		private async static Task<Dictionary<string, List<Dictionary<string, object>>>> CreateJson(Dictionary<string, List<string>> sherdogLinksDictionary)
 		{
+			// Initialize dictionary representing the json
 			Dictionary<string, List<Dictionary<string, object>>> json = new Dictionary<string, List<Dictionary<string, object>>>();
+
+			// Looping through weighclasses
 			foreach (var weightClass in sherdogLinksDictionary)
 			{
+				// Initialize weightclass fighters dictionary
 				List<Dictionary<string, object>> weightClassDictionary = new List<Dictionary<string, object>>();
 
+				// Limit the tasks to weightClass.Count (around 15-16 fighters)
 				int maxDegreeOfParallelism = weightClass.Value.Count;
+
 				SemaphoreSlim semaphore = new SemaphoreSlim(maxDegreeOfParallelism);
 				List<Task> tasks = new List<Task>();
 
+				// Looping through each fighter in weightClass list
 				foreach (var fighter in weightClass.Value)
 				{
+					// Add task
 					await semaphore.WaitAsync();
 					tasks.Add(Task.Run(async () =>
 					{
 						try
 						{
+							// Scrap fighter from sherdog
 							var fighterDictionary = await ScrapSherdogStats(fighter);
+
+							// Add fighter to weightclass
 							weightClassDictionary.Add(fighterDictionary);
 						}
 						catch (Exception ex)
@@ -209,13 +316,16 @@ namespace UfcStatsAPI.Services
 						}
 						finally
 						{
+							// Release task
 							semaphore.Release();
 						}
-
 					}));
 				}
 
+				// Stop until all tasks are done
 				await Task.WhenAll(tasks);
+
+				// Add weightClass name as key and 
 				json.Add(weightClass.Key, weightClassDictionary);
 			}
 
